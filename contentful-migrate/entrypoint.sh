04ff6a503e26f45fa470a4c0df7265d02adbc2ff
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-01-08 18:37:07 -0500

    Put folder check back

--- contentful-migrate/entrypoint.sh
+++ contentful-migrate/entrypoint.sh
@@ -13,4 +13,17 @@ then
     exit 1
 fi
 
+
+if [[ -z $contentfulMigrationLocation ]];
+then
+    echo "Location for folder containing migration files required. Provide value in contentfulMigrationLocation variable" >&2
+    exit 1
+fi
+
+if [[ ! -d "$contentfulMigrationLocation" &&  ! -f "$contentfulMigrationLocation" ]];
+then
+    echo "Folder location '$contentfulMigrationLocation' does not exist."
+    exit 1
+fi
+
 echo "Initiating Migrations"
