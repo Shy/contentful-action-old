816bc2ad28cc399df74986fe3825bc39ff146d0d
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-01-08 11:06:46 -0500

    Added new action

---
+++ contentful-migrate/entrypoint.sh
@@ -0,0 +1,17 @@
+set -e
+
+if [ -z "$spaceId" ];
+then
+    echo "\$spaceId is not set." >&2
+    exit 1
+fi
+
+if [ -z "$accessToken" ];
+then
+    echo "\$accessToken is not set." >&2
+    exit 1
+fi
+
+echo "Initiating Migrations"
+
+node migrate $spaceId $GITHUB_REF $accessToken
