19753d0a5773bd9c4e753ebfb1588b71839267e6
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-01-08 12:22:42 -0500

    sync repo and local

--- netlify-rebuild/entrypoint.sh
+++ netlify-rebuild/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if [ -z "$netlifyBuildHook" ];
+then
+    echo "\$netlifyBuildHook is not set." >&2
+    exit 1
+fi
+
 if test "$GITHUB_REF" = 'refs/heads/master'; then
     sh -c "echo 'Triggering rebuild on Netlify via webhook.'"
     sh -c "curl -X POST -d {} $netlifyBuildHook"
